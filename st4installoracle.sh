6ced684e64237873ac82eff4d4e16b8dd5123560
fxl <fxl0206@gmail.com>
2015-06-29 12:35:24 +0800

    Update st4installoracle.sh

--- st4installoracle.sh
+++ st4installoracle.sh
@@ -1,5 +1,5 @@
 source /etc/profile
-su - oracle -c "cd /data/database && ./runInstaller -silent -ignorePrereq -responseFile /etc/db_install.rsp"
+su - oracle -c "source /etc/profile && cd /data/database && ./runInstaller -silent -ignorePrereq -responseFile /etc/db_install.rsp"
 while true
 do
    v_count=`ps -aux | grep install | grep java | grep -v grep |wc -l`
@@ -32,7 +32,7 @@ done
 /u01/app/oracle/oraInventory/orainstRoot.sh
 /u01/app/oracle/product/11.2.0/db_1/root.sh
 
-su - oracle -c "netca /silent /responsefile /etc/netca.rsp"
+su - oracle -c "source /etc/profile && netca /silent /responsefile /etc/netca.rsp"
 while true
 do
    v_count=`ps -aux | grep netca | grep -v grep | grep java |wc -l`
@@ -44,9 +44,9 @@ do
    sleep 10
    fi
 done
-su - oracle -c "lsnrctl status"
+su - oracle -c "source /etc/profile && lsnrctl status"
 sleep 5
-su - oracle -c "dbca -silent -responseFile /etc/dbca.rsp"
-
+su - oracle -c "source /etc/profile && dbca -silent -responseFile /etc/dbca.rsp"
+source /etc/profile
 ps -ef | grep ora_ | grep -v grep
 lsnrctl status
