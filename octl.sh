849657785fbab6a41c6ac400b42ad3bd93a5e5fd
fxl <fxl0206@gmail.com>
2015-06-29 13:50:23 +0800

    Update octl.sh

--- octl.sh
+++ octl.sh
@@ -27,26 +27,19 @@ install(){
   yum install -y binutils compat-libcap1 compat-libstdc++-33 gcc-c++ glibc glibc-devel 
   yum install -y ksh libgcc libstdc++ libstdc++-devel libaio libaio-devel make sysstat unixODBC unixODBC-devel
   echo "init Env.................."
-  export TMP=/tmp
-  export TMPDIR=$TMP
-  export ORACLE_BASE=/u01/app/oracle
-  export ORACLE_HOME=$ORACLE_BASE/product/11.2.0/db_1
-  export ORACLE_SID=orcl
-  export ORACLE_TERM=xterm
-  export PATH=$ORACLE_HOME/bin:/usr/sbin:$PATH
-  export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib
-  export CLASSPATH=$ORACLE_HOME/JRE:$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib
+
   echo "export TMP=/tmp" >> /etc/profile
-  echo "export TMPDIR=$TMP" >> /etc/profile
+  echo "export TMPDIR=\$TMP" >> /etc/profile
   echo "export ORACLE_BASE=/u01/app/oracle" >> /etc/profile
-  echo "export ORACLE_HOME=$ORACLE_BASE/product/11.2.0/db_1" >> /etc/profile
+  echo "export ORACLE_HOME=\$ORACLE_BASE/product/11.2.0/db_1" >> /etc/profile
   echo "export ORACLE_SID=orcl" >> /etc/profile
   echo "export ORACLE_TERM=xterm" >> /etc/profile
-  echo "export PATH=$ORACLE_HOME/bin:/usr/sbin:$PATH" >> /etc/profile
-  echo "export LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib" >> /etc/profile
-  echo "export CLASSPATH=$ORACLE_HOME/JRE:$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib" >> /etc/profile
+  echo "export PATH=\$ORACLE_HOME/bin:/usr/sbin:\$PATH" >> /etc/profile
+  echo "export LD_LIBRARY_PATH=\$ORACLE_HOME/lib:/lib:/usr/lib" >> /etc/profile
+  echo "export CLASSPATH=\$ORACLE_HOME/JRE:\$ORACLE_HOME/jlib:\$ORACLE_HOME/rdbms/jlib" >> /etc/profile
   source /etc/profile
   echo $ORACLE_SID
+  echo "init Env success~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   
   echo "oracle soft nproc 2047" >> /etc/security/limits.conf
   echo "oracle hard nproc 16384" >> /etc/security/limits.conf
