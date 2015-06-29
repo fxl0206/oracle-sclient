6e07cf84e2e105cc00c9f3e0f539e40c91c15148
fxl <fxl0206@gmail.com>
2015-06-29 12:31:26 +0800

    Update st4installoracle.sh

--- st4installoracle.sh
+++ st4installoracle.sh
@@ -2,7 +2,7 @@ source /etc/profile
 su - oracle -c "cd /data/database && ./runInstaller -silent -ignorePrereq -responseFile /etc/db_install.rsp"
 while true
 do
-   v_count=`ps -aux | grep db_install.rsp | grep -v grep |wc -l`
+   v_count=`ps -aux | grep install | grep java | grep -v grep |wc -l`
    if [ $v_count -eq  0 ]
    then
    break
@@ -16,12 +16,16 @@ while true
 do
    if [ -f /u01/app/oracle/oraInventory/orainstRoot.sh ]; then
       break
+   else
+      echo "is installing database,sleep 10 minues!"
    fi
 done
 while true
 do
    if [ -f /u01/app/oracle/product/11.2.0/db_1/root.sh ]; then
       break
+   else
+      echo "is installing database,sleep 10 minues!"
    fi
 done
 
