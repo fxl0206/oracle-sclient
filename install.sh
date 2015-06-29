04237a57d07f373c4520b26f54461d73d9bc38a3
fxl0206 <fxl0206@gmail.com>
2015-06-28 22:40:44 -0400

    modified:   install.sh 	modified:   st4installoracle.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#./st0instalrpm.sh
-#./st1setEnv.sh
-#./st3inituser.sh
+./st0instalrpm.sh
+./st1setEnv.sh
+./st3inituser.sh
 ./st4installoracle.sh
--- st4installoracle.sh
+++ st4installoracle.sh
@@ -1,4 +1,4 @@
-#su - oracle -c "cd /data/database && ./runInstaller -silent -ignorePrereq -responseFile /etc/db_install.rsp"
+su - oracle -c "cd /data/database && ./runInstaller -silent -ignorePrereq -responseFile /etc/db_install.rsp"
 /u01/app/oracle/oraInventory/orainstRoot.sh
 /u01/app/oracle/product/11.2.0/db_1/root.sh
 
